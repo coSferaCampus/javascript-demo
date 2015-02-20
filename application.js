bb643dd5c539621f20be0fe04e41911fbc3e9fc2
Javier Aranda <jaranda@nosolosoftware.es>
2015-02-20 14:57:25 +0100

    Ejercicio de ajax resuelto

--- application.js
+++ application.js
@@ -1,16 +1,29 @@
+var count = 1;
+
+function addRowToTable(book) {
+  var table = $("table tbody");
+  var html = "";
+
+  html += '<tr>';
+  html += '<th scope="row">' + count + '</th>';
+  html += '<td>' + book.title + '</td>';
+  html += '<td>' + book.author + '</td>';
+  html += '<td>' + book.price + '</td>';
+  html += '</tr>';
+
+  table.append(html);
+  count++;
+}
+
 $(function() {
-  $("button.btn").click(function(evt) {
-    var e = $(evt.target);
-    var panel = $(".panel");
-
-    var tipos = ["default", "primary", "success", "info", "warning", "danger"];
-
-    $.each(tipos, function(index, tipo) {
-      if ( e.hasClass("btn-" + tipo) ) {
-        panel.removeClass();
-        panel.addClass('panel');
-        panel.addClass("panel-" + tipo);
-      }
+  // cargar el listado inicial
+  $.ajax({
+    url: "http://127.0.0.1:3000/books",
+    method: "GET",
+    dataType: "json"
+  }).done(function(books) {
+    _.forEach(books, function(book) {
+      addRowToTable(book);
     });
   });
 });
